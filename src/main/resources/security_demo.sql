67bd1f95b9b349dc751e4a340e6b611db8d16159
ryan.miao <ryan.miao@nf-3.com>
2018-04-28 18:20:12 +0800

    添加db备份

---
+++ src/main/resources/security_demo.sql
@@ -0,0 +1,116 @@
+/*
+ Navicat PostgreSQL Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : PostgreSQL
+ Source Server Version : 100003
+ Source Host           : localhost:5432
+ Source Catalog        : security_demo
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 100003
+ File Encoding         : 65001
+
+ Date: 28/04/2018 18:18:46
+*/
+
+
+-- ----------------------------
+-- Sequence structure for hibernate_sequence
+-- ----------------------------
+DROP SEQUENCE IF EXISTS "public"."hibernate_sequence";
+CREATE SEQUENCE "public"."hibernate_sequence" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 9223372036854775807
+START 1
+CACHE 1;
+
+-- ----------------------------
+-- Table structure for room
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."room";
+CREATE TABLE "public"."room" (
+  "id" int4 NOT NULL,
+  "comment" varchar(255) COLLATE "pg_catalog"."default",
+  "create_date" timestamp(6),
+  "name" varchar(200) COLLATE "pg_catalog"."default" NOT NULL,
+  "update_date" timestamp(6)
+)
+;
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."sys_role";
+CREATE TABLE "public"."sys_role" (
+  "id" int8 NOT NULL,
+  "name" varchar(255) COLLATE "pg_catalog"."default"
+)
+;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO "public"."sys_role" VALUES (1, 'ROLE_ADMIN');
+INSERT INTO "public"."sys_role" VALUES (2, 'ROLE_USER');
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."sys_user";
+CREATE TABLE "public"."sys_user" (
+  "id" int8 NOT NULL,
+  "password" varchar(255) COLLATE "pg_catalog"."default",
+  "username" varchar(255) COLLATE "pg_catalog"."default"
+)
+;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO "public"."sys_user" VALUES (2, '76bd7b432856c9ea6ce558cba9d955ff', 'test');
+INSERT INTO "public"."sys_user" VALUES (1, '25f214e1c1b400421a8ada8a213c9fcb', 'admin');
+
+-- ----------------------------
+-- Table structure for sys_user_roles
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."sys_user_roles";
+CREATE TABLE "public"."sys_user_roles" (
+  "sys_user_id" int8 NOT NULL,
+  "roles_id" int8 NOT NULL
+)
+;
+
+-- ----------------------------
+-- Records of sys_user_roles
+-- ----------------------------
+INSERT INTO "public"."sys_user_roles" VALUES (1, 1);
+INSERT INTO "public"."sys_user_roles" VALUES (2, 2);
+
+-- ----------------------------
+-- Alter sequences owned by
+-- ----------------------------
+SELECT setval('"public"."hibernate_sequence"', 2, false);
+
+-- ----------------------------
+-- Primary Key structure for table room
+-- ----------------------------
+ALTER TABLE "public"."room" ADD CONSTRAINT "room_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table sys_role
+-- ----------------------------
+ALTER TABLE "public"."sys_role" ADD CONSTRAINT "sys_role_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table sys_user
+-- ----------------------------
+ALTER TABLE "public"."sys_user" ADD CONSTRAINT "sys_user_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Foreign Keys structure for table sys_user_roles
+-- ----------------------------
+ALTER TABLE "public"."sys_user_roles" ADD CONSTRAINT "fkd0ut7sloes191bygyf7a3pk52" FOREIGN KEY ("sys_user_id") REFERENCES "public"."sys_user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."sys_user_roles" ADD CONSTRAINT "fkdpvc6d7xqpqr43dfuk1s27cqh" FOREIGN KEY ("roles_id") REFERENCES "public"."sys_role" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
