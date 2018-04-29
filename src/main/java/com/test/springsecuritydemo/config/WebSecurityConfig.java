057db5fa013278669733b1f7b3468cf3f15ed2f9
Ryan <woshimrf@126.com>
2018-04-29 14:46:09 +0800

    修改跨域配置方案； 新增csrf配置（如果选择jwt之后会选择关闭）

--- src/main/java/com/test/springsecuritydemo/config/WebSecurityConfig.java
+++ src/main/java/com/test/springsecuritydemo/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.test.springsecuritydemo.config;
 
 import com.test.springsecuritydemo.constant.AuthorityEnum;
+import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableGlobalMethodSecurity(securedEnabled = true)// 控制权限注解
 @Configuration
@@ -42,24 +46,38 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // @formatter:off
         http.headers().frameOptions().disable()
+            // 针对Cross Site Request Forgery(跨站请求伪造)开启token验证，即除了get之外的http动词都将被这个
+            //e.g. Set-Cookie: XSRF-TOKEN=89d9a369-553b-45f2-8fd0-1114dd5cd01f; Path=/
+            //则post等请求必须携带header X-XSRF-TOKEN=这个值;
+            //这个可以防止用户打开本网站后，未关闭浏览器而访问钓鱼网站post正规请求。见https://docs.spring.io/spring-security/site/docs/current/reference/html/csrf.html#csrf-cookie
+            .and().cors()
             .and()
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
             .and()
-                .authorizeRequests()
-                .antMatchers("/actuator/health").permitAll()
-                .antMatchers("/actuator/**").hasAuthority(AuthorityEnum.ROLE_ADMIN.name())
-                .antMatchers("/login*").permitAll()
-                .anyRequest().fullyAuthenticated()
+            .authorizeRequests()
+            .antMatchers("/actuator/health").permitAll()
+            .antMatchers("/actuator/**").hasAuthority(AuthorityEnum.ROLE_ADMIN.name())
+            .antMatchers("/login*").permitAll()
+            .anyRequest().fullyAuthenticated()
             .and()
-                .formLogin().loginPage("/login").failureUrl("/login?error")
+            .formLogin().loginPage("/login").failureUrl("/login?error")
             .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
             .and()
-                .exceptionHandling().accessDeniedPage("/access?error")
+            .exceptionHandling().accessDeniedPage("/access?error")
             .and()
-                .exceptionHandling().authenticationEntryPoint(new Http403ForbiddenEntryPoint())
+            .exceptionHandling().authenticationEntryPoint(new Http403ForbiddenEntryPoint())
             .and()
-                .httpBasic();
-        // @formatter:on
+            .httpBasic();
+            // @formatter:on
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("http://corshost:9095"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/java/com/test/springsecuritydemo/constant/AuthorityEnum.java
+++ src/main/java/com/test/springsecuritydemo/constant/AuthorityEnum.java
@@ -1,5 +1,6 @@
 package com.test.springsecuritydemo.constant;
 
 public enum  AuthorityEnum {
-    ROLE_ADMIN
+    ROLE_ADMIN,
+    ROLE_USER,
 }
