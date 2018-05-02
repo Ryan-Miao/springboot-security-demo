ace3f9d74518c0490ef46864d14f9c7531cb3d04
ryan.miao <ryan.miao@nf-3.com>
2018-05-02 09:02:49 +0800

    重组security模块，聚合到一起

--- src/main/java/com/test/springsecuritydemo/security/AuthenticationException.java
+++ src/main/java/com/test/springsecuritydemo/security/AuthenticationException.java
@@ -1,19 +1,9 @@
 package com.test.springsecuritydemo.security;
 
-import java.io.Serializable;
-import lombok.Value;
-
 public class AuthenticationException extends RuntimeException {
+
     public AuthenticationException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    @Value
-    public static class JwtAuthenticationRequest implements Serializable {
-
-        private static final long serialVersionUID = -8445943548965154778L;
-
-        private String username;
-        private String password;
-    }
 }
---
+++ src/main/java/com/test/springsecuritydemo/security/vo/JwtAuthenticationRequest.java
@@ -0,0 +1,11 @@
+package com.test.springsecuritydemo.security.vo;
+
+import java.io.Serializable;
+
+public class JwtAuthenticationRequest implements Serializable {
+
+    private static final long serialVersionUID = -8445943548965154778L;
+
+    private String username;
+    private String password;
+}
